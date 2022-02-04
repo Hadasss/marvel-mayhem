bf15cfb3e2f5f9a1576b6affa258651334f85f12
Hadasss <hadas.gadish@gmail.com>
2022-02-04 11:46:38 -0700

    added fetch for gif api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-// bookSearchBtn.addEventListener("click", bookInputHandler);
 const searchBtn = document.querySelector(".search-bttn");
 let formInput = document.querySelector("#book-name");
 const heroNameDisplay = document.querySelector(".hero-name-display");
+let heroGif = document.querySelector("hero-gif");
 const buttonsDiv = document.querySelector(".buttons");
 const comicsBtn = document.querySelector(".comics");
 const storiesBtn = document.querySelector(".stories");
 const eventsBtn = document.querySelector(".events");
 
+// fetch request to display hero by search key. Marvel API.
+// dynamically generating elements to display user choice.
 const getfetchResponse = function (searchInput) {
   let url =
     "https://gateway.marvel.com:443/v1/public/characters?name=" +
@@ -24,10 +26,11 @@ const getfetchResponse = function (searchInput) {
           let heroDescriptionP = document.createElement("p");
           // heroDescriptionP.setAttribute("class", "");
           heroDescriptionP.textContent = data.data.results[0].description;
+
+          getHeroGif(searchInput);
+
           heroNameDisplay.appendChild(heroNameTitle);
           heroNameDisplay.appendChild(heroDescriptionP);
-
-          // comicsBtnDisplay();
         });
       }
     })
@@ -36,6 +39,33 @@ const getfetchResponse = function (searchInput) {
     });
 };
 
+// function to generate hero gif. GIPHY API
+const getHeroGif = function (searchInput) {
+  let gifUrl =
+    "https://api.giphy.com/v1/gifs/search?api_key=S3HuUjpb6Y7vXd6wE7kLLaqZ5hY4QeZC&q=" +
+    searchInput;
+
+  fetch(gifUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data, searchInput);
+          // heroNameDisplay.appendChild(gifContainerDiv); // BUG!!
+          let gifRandomIndex = Math.floor(Math.random() * 50);
+          console.log(gifRandomIndex);
+          let gifSrc = `${data.data[gifRandomIndex].url}.gif`;
+          console.log(gifSrc);
+          heroGif.setAttribute("src", gifSrc);
+        });
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+//  function to fetch data for hero additional info: comics, stories, events. data retrieved from Matvel API.
+// dynamically generating elements for display.
 const comicsBtnDisplay = function () {
   const comicsDiv = document.createElement("div");
   // comicsDiv.setAttribute("class", "");
@@ -46,16 +76,16 @@ const comicsBtnDisplay = function () {
   // create a for loop to loop through array of comic books. display first 10 results.
   let comicBookLi = document.createElement("li");
 
-  // append outside foor loop
   comicsTitle.appendChild(comicsDiv);
   comicsDiv.appendChild(comicsUl);
 
   buttonsDiv.appendChild(comicsDiv);
 };
 
+// function to handle user input for first fetch.
 const InputHandler = function (event) {
   event.preventDefault();
-  console.log("clicked");
+  // heroNameDisplay.innerHTML = "";
   let searchInput = formInput.value.trim();
 
   if (searchInput) {
