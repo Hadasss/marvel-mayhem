a41559bb3c18f6be294c1ccd1a52d4a26c145f05
Hadasss <hadas.gadish@gmail.com>
2022-02-03 22:45:45 -0700

    buttons div structure in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@
 const searchBtn = document.querySelector(".search-bttn");
 let formInput = document.querySelector("#book-name");
 const heroNameDisplay = document.querySelector(".hero-name-display");
+const buttonsDiv = document.querySelector(".buttons");
+const comicsBtn = document.querySelector(".comics");
+const storiesBtn = document.querySelector(".stories");
+const eventsBtn = document.querySelector(".events");
 
 const getfetchResponse = function (searchInput) {
   let url =
@@ -16,22 +20,34 @@ const getfetchResponse = function (searchInput) {
           console.log(data, searchInput);
           let heroNameTitle = document.createElement("h3");
           // heroNameTitle.setAttribute("class", "");
-          heroNameTitle.textContent = data.data.results[0].name.text;
-          const heroNameDiv = document.createElement("div");
-          // heroNameDiv.setAttribute("class", "");
-
+          heroNameTitle.textContent = data.data.results[0].name;
           let heroDescriptionP = document.createElement("p");
-          heroDescriptionP.textContent = data.data.results[0].description.value;
-
-          heroNameDiv.appendChild(heroNameTitle);
-          heroNameDiv.appendChild(heroDescriptionP);
-          heroNameDisplay.appendChild(heroNameDiv);
+          // heroDescriptionP.setAttribute("class", "");
+          heroDescriptionP.textContent = data.data.results[0].description;
+          heroNameDisplay.appendChild(heroNameTitle);
+          heroNameDisplay.appendChild(heroDescriptionP);
         });
       }
     })
     .catch(function (error) {
       console.log(error);
     });
+};
+
+const comicsBtnDisplay = function () {
+  const comicsDiv = document.createElement("div");
+  // comicsDiv.setAttribute("class", "");
+  const comicsTitle = document.createElement("h3");
+  comicsTitle.textContent = "Your hero appeared in these issues:";
+  const comicsUl = document.createElement("ul");
+
+  // create a for loop to loop through array of comic books. display first 10 results.
+  let comicBookLi = document.createElement("li");
+
+  comicsTitle.appendChild(comicsDiv);
+  comicsDiv.appendChild(comicsUl);
+
+  buttonsDiv.appendChild(comicsDiv);
 };
 
 const InputHandler = function (event) {
@@ -48,4 +64,5 @@ const InputHandler = function (event) {
   }
 };
 
+comicsBtn.addEventListener("click", comicsBtnDisplay);
 searchBtn.addEventListener("click", InputHandler);
