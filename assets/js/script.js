2c72df1c55f7751733b7cba413c9d38b331aa7d9
Hadasss <hadas.gadish@gmail.com>
2022-02-03 21:29:41 -0700

    script.js tag added, basic fetch added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // bookSearchBtn.addEventListener("click", bookInputHandler);
-const searchBtn = document.querySelector("#search-bttn");
+const searchBtn = document.querySelector(".search-bttn");
 let formInput = document.querySelector("#book-name");
 const heroNameDisplay = document.querySelector(".hero-name-display");
 
@@ -36,6 +36,7 @@ const getfetchResponse = function (searchInput) {
 
 const InputHandler = function (event) {
   event.preventDefault();
+  console.log("clicked");
   let searchInput = formInput.value.trim();
 
   if (searchInput) {
